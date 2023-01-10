5aedce014b01f6e73fa24c2f056cda99c785ccb2
robyrt1 <robertpinto1190.rpm@gmail.com>
2023-01-10 15:53:07 -0400

    ajuste gitignore

--- src/routers/index.js
+++
@@ -1,5 +0,0 @@
-const laudos = require("../app/ExamReports/ReportsPdf.router");
-const laboratoryTest = require("../app/LaboratoryTest/LaboratoryTest.router");
-const router = [laudos,laboratoryTest ];
-
-module.exports = router;
--- src/shared/httpResponseMappingHandler.shared.js
+++
@@ -1,7 +0,0 @@
-module.exports = function httpResponseMappingHandlerShared(
-  status,
-  dados,
-  message
-) {
-  return { status, dados, message };
-};
